a2aa8c3ea67ceda23af2a64a62561c418e49a935
João Falcão <jgabfalcao@gmail.com>
2023-02-09 20:16:25 -0300

    Creating state for input

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
+import React from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card";
+import { useState } from "react";
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
       <Card name="João" time="10:55:25" />
       <Card name="Rodrigo" time="11:10:10" />
